267f0aa11de039f57d34a596e4edf38ff51c49d8
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-05-08 14:34:33 +0300

    Done control on day-1

---
+++ day-2/index.js
@@ -0,0 +1,6 @@
+const circles=document.querySelectorAll(".circle");
+const buttons=document.querySelectorAll(".btn");
+const progress=document.querySelector(".progress");
+
+console.log(progress,circles,buttons);
+
