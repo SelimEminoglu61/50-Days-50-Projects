8fac533b024993eb48dc908b25a74f00c7be4b70
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-05-16 16:29:37 +0300

    Completed day-2

--- day-2/index.js
+++ day-2/index.js
@@ -1,66 +1,82 @@
-const circles=document.querySelectorAll(".circle");
-const prevButton=document.querySelector("#prev");
-const nextButton=document.querySelector("#next");
-const progress=document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+const progress = document.getElementById("progress");
 
+prevButton.addEventListener("click", prevButtonClicked);
+nextButton.addEventListener("click", nextButtonClicked);
 
-prevButton.addEventListener("click",prevButtonClicked);
-nextButton.addEventListener("click",nextButtonClicked);
+let noneActive = 0;
+let active = 0;
 
-let noneActive=0;
-let active=0;
-
-function prevButtonClicked(){
-
-    circles.forEach((circle)=>{
-        circle.classList.forEach((item)=>{
-            if(item=='active'){
-                active-=1;
-            }
-        })
-    
-    })
-    noneActive=circles.length-active;
-    progress.getAttribute("style");
-    if(active==4){
-        progress.style.width = "66.6667%";
-        nextButton.removeAttribute("disabled");
-        
-    }
-    else if(active==3){
-        progress.style.width = "33.3333%";
-    }
-    else if(active==2){
-        progress.style.width = "0%";
-        prevButton.setAttribute("disabled","");
-        active=0;
-    }
-
+function prevButtonClicked() {
+  circles.forEach((circle) => {
+    circle.classList.forEach((item) => {
+      if (item == "active") {
+        active -= 1;
+      }
+    });
+  });
+  noneActive = circles.length - active;
+  progress.getAttribute("style");
+  if (noneActive === 2) {
+    progress.style.width = "66.6667%";
+    nextButton.removeAttribute("disabled");
+    removeActiveClass();
+  }
+  if (noneActive == 5) {
+    progress.style.width = "33.3333%";
+    removeActiveClass();
+  }
+  if (noneActive == 7) {
+    progress.style.width = "0%";
+    prevButton.setAttribute("disabled", "");
+    removeActiveClass();
+    active = 0;
+  }
 }
 
-function nextButtonClicked(){
-   
-    circles.forEach((circle)=>{
-        circle.classList.forEach((item)=>{
-            if(item=='active'){
-                active+=1;
-            }
-        })
-    })
-    noneActive=circles.length-active;
-    progress.getAttribute("style");
-    if(noneActive==3){
-        progress.style.width = "33.3333%";
-        prevButton.removeAttribute("disabled");
-        
+function nextButtonClicked() {
+  circles.forEach((circle) => {
+    if (circle.className == "circle active") {
+      active += 1;
     }
-    else if(noneActive==2){
-        progress.style.width = "66.6667%";
+  });
+  noneActive = circles.length - active;
+  progress.getAttribute("style");
+  if (active == 1) {
+    progress.style.width = "33.3333%";
+    prevButton.removeAttribute("disabled");
+    active = circles.length - noneActive;
+    addActiveClass();
+  }
+  if (active == 3) {
+    progress.style.width = "66.6667%";
+    active = circles.length - noneActive;
+    addActiveClass();
+  }
+  if (active == 6) {
+    progress.style.width = "100%";
+    nextButton.setAttribute("disabled", "");
+    addActiveClass();
+    active = 6;
+  }
+}
+
+function addActiveClass() {
+  for (let i = 0; i < circles.length; i++) {
+    if (circles[i].className == "circle") {
+      circles[i].classList.add("active");
+      break;
     }
-    else if(noneActive==1){
-        progress.style.width = "100%";
-        nextButton.setAttribute("disabled","");
-        active=5;
+  }
+}
+
+function removeActiveClass() {
+  for (let i = circles.length - 1; i > 0; i--) {
+    if (circles[i].className == "circle active") {
+      circles[i].classList.remove("active");
+      break;
     }
-    
+  }
 }
